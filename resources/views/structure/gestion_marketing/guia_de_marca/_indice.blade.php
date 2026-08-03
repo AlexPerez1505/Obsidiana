19441ce85d837f414c41e8fc8acd9d3d731be6c6
marinasher-svg <al222410862@gmail.com>
2026-08-03 10:53:03 -0600

    Cambios~

--- resources/views/structure/gestion_marketing/guia_de_marca/_indice.blade.php
+++
@@ -1,9 +0,0 @@
-<aside class="gm-index">
-    <div class="gm-index__title">Contenido</div>
-    <nav class="gm-index__list">
-        <a href="#paleta" class="gm-index__link active" data-target="paleta">1. Paleta de color</a>
-        <a href="#tipografia" class="gm-index__link" data-target="tipografia">2. Tipografía</a>
-        <a href="#tono" class="gm-index__link" data-target="tono">3. Tono de voz</a>
-        <a href="#legal" class="gm-index__link" data-target="legal">4. Leyenda legal</a>
-    </nav>
-</aside>
--- routes/web/marketing.php
+++ routes/web/marketing.php
@@ -1,35 +1,7 @@
 <?php
 
-use App\Http\Controllers\TaskController;
-use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Gestión de Marketing
 |--------------------------------------------------------------------------
 */
-Route::middleware(['auth', 'verified', 'approved'])->group(function () {
-    Route::get('/marketing/tareas', [TaskController::class, 'index'])
-        ->name('marketing.tareas.index');
-
-    Route::get('/marketing/tareas/crear', [TaskController::class, 'create'])
-        ->name('marketing.tareas.create');
-
-    Route::post('/marketing/tareas', [TaskController::class, 'store'])
-        ->name('marketing.tareas.store');
-
-    Route::put('/marketing/tareas/{task}', [TaskController::class, 'update'])
-        ->name('marketing.tareas.update');
-
-    Route::get('/marketing/aprobacion-flyers', [TaskController::class, 'aprobacionFlyers'])
-        ->name('marketing.aprobacion_flyers.index');
-
-    Route::get('/marketing/agenda', [TaskController::class, 'agenda'])
-        ->name('marketing.agenda.index');
-
-    Route::get('/marketing/biblioteca-catalogo', [TaskController::class, 'bibliotecaCatalogo'])
-        ->name('marketing.biblioteca_catalogo.index');
-
-    Route::get('/marketing/guia-de-marca', [TaskController::class, 'guiaDeMarca'])
-        ->name('marketing.guia_de_marca.index');
-});
